eb5b0b9f1549e76cbb98565eb9c029e460d8cef2
Praneeth-Kallakuri <praneethkallakuri22@gmail.com>
2024-12-30 11:57:47 +0530

    PRE-KDU-TYPESCRIPT: Updated Typescript files with codes

--- typescript/q3.ts
+++ typescript/q3.ts
@@ -5,3 +5,35 @@
 
   Export the filterArray function so that the code can be tested in the test file.
 */
+export type User = {
+    id: number;
+    name: string;
+    email: string;
+    role?: string;
+};
+export interface Admin extends User {
+    permissions: string[];
+}
+
+export function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+    return obj[key];
+}
+
+export enum Status {
+    Active = "Active",
+    Inactive = "Inactive",
+    Suspended = "Suspended"
+}
+
+export type UserStatus = [User, Status];
+
+// Function to print user status
+export function printUserStatus(userStatus: UserStatus): void {
+    const [user, status] = userStatus;
+    console.log(`${user.name} is currently ${status}.`);
+}
+
+//Generic function to filter an array based on a predicate function
+export function filterArray<T>(arr: T[], predicate: (item: T) => boolean): T[] {
+    return arr.filter(predicate);
+}
